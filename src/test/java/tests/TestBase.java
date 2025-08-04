e1e920a67d913610ca5b3cd4b8d0f53872497f10
Evgeniya Malysheva <cotopilla@yandex.ru>
2025-08-04 22:13:27 +0300

    add media

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,14 +1,11 @@
 package tests;
 
-import com.codeborne.selenide.logevents.SelenideLogger;
 import config.BookingConfig;
 import config.ConfigReader;
-import io.qameta.allure.selenide.AllureSelenide;
 import io.restassured.RestAssured;
 import models.responses.BookingModel;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
@@ -21,11 +18,6 @@ public class TestBase {
     @BeforeAll
     public static void setApiParams() {
         RestAssured.baseURI = config.getBaseURI();
-    }
-
-    @BeforeEach
-    public void addAllureSelenideListener() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
